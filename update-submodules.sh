fbb0ea733805347868cd39b7fc9303513fe95ca6
Cyber Oliveira <lccro@msn.com>
2022-07-08 10:35:59 -0300

    improving update script

--- update-submodules.sh
+++ update-submodules.sh
@@ -8,5 +8,5 @@ echo initting submodules:
 git submodule update --init --recursive
 
 echo pulling latest:
-git submodule foreach "git checkout -q `git remote show origin | sed -n '/HEAD branch/s/.*: //p'` ; git pull -q"
+git submodule foreach "git checkout -q $(git remote show origin | sed -n '/HEAD branch/s/.*: //p') ; git pull -q"
 
