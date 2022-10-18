9ea29db7fd8409057862cb342e7ad8179620e8e7
Cyber Oliveira <lccro@msn.com>
2022-10-18 15:45:56 -0300

    adds git_main_branch function inside of update-submodules.sh (3)

--- update-submodules.sh
+++ update-submodules.sh
@@ -13,12 +13,12 @@ function git_main_branch() {
   echo master
 }
 
-echo updating:
+echo "--- updating:"
 git pull
 
-echo initting submodules:
+echo "--- initting submodules:"
 git submodule update --init --recursive
 
-echo pulling latest:
+echo "--- pulling latest:"
 git submodule foreach "git checkout -q $(git_main_branch) ; git branch; git pull -q"
 
