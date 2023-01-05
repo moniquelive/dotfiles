0229f80c2d4ab1ea399182e152d0bbe146c697cf
Cyber Oliveira <lccro@msn.com>
2023-01-05 11:21:29 -0300

    adds luasnip snippets && sets a version for toggleterm && removes conflicting mapping

--- config/nvim.symlink/lua/plugins.lua
+++ config/nvim.symlink/lua/plugins.lua
@@ -104,7 +104,7 @@ do
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<F4>', vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<F3>', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
