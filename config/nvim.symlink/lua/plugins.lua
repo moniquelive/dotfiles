c9974f6bcaa37532dc933f346faef9a85cdc1e31
Cyber Oliveira <lccro@msn.com>
2023-02-13 10:49:55 -0300

    moved null-ls block inside of lspconfig's

--- config/nvim.symlink/lua/plugins.lua
+++ config/nvim.symlink/lua/plugins.lua
@@ -1,8 +1,8 @@
 ------------------------------------------------------------- ToggleTerm {{{1
 do
   require("toggleterm").setup {
-    direction = 'horizontal',
-    open_mapping = '<leader>tt',
+      direction = 'horizontal',
+      open_mapping = '<leader>tt',
   }
   function _G.set_terminal_keymaps()
     local opts = { buffer = 0 }
@@ -17,17 +17,17 @@ do
 
   local Terminal  = require('toggleterm.terminal').Terminal
   local gotestsum = Terminal:new {
-    cmd = "gotestsum --watch -f dots-v2",
-    hidden = true,
-    direction = 'horizontal',
-    ---@diagnostic disable-next-line: unused-local
-    on_open = function(term)
-      local winnr = vim.fn['winnr']('#')
-      local winid = vim.fn['win_getid'](winnr)
-      vim.fn['win_gotoid'](winid)
-      vim.cmd('stopinsert')
-    end
-  }
+          cmd = "gotestsum --watch -f dots-v2",
+          hidden = true,
+          direction = 'horizontal',
+          ---@diagnostic disable-next-line: unused-local
+          on_open = function(term)
+            local winnr = vim.fn['winnr']('#')
+            local winid = vim.fn['win_getid'](winnr)
+            vim.fn['win_gotoid'](winid)
+            vim.cmd('stopinsert')
+          end
+      }
   function _G._gotestsum_toggle()
     gotestsum:toggle()
   end
@@ -40,21 +40,21 @@ do
   require 'refactoring'.setup {}
   -- Remaps for the refactoring operations currently offered by the plugin
   vim.api.nvim_set_keymap("v", "<leader>re",
-    [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]],
-    { noremap = true, silent = true, expr = false })
+      [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]],
+      { noremap = true, silent = true, expr = false })
   vim.api.nvim_set_keymap("v", "<leader>rf",
-    [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]],
-    { noremap = true, silent = true, expr = false })
+      [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]],
+      { noremap = true, silent = true, expr = false })
   vim.api.nvim_set_keymap("v", "<leader>rv",
-    [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]],
-    { noremap = true, silent = true, expr = false })
+      [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]],
+      { noremap = true, silent = true, expr = false })
   vim.api.nvim_set_keymap("v", "<leader>ri",
-    [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
-    { noremap = true, silent = true, expr = false })
+      [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
+      { noremap = true, silent = true, expr = false })
 
   -- Inline variable can also pick up the identifier currently under the cursor without visual mode
   vim.api.nvim_set_keymap("n", "<leader>ri", [[ <Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
-    { noremap = true, silent = true, expr = false })
+      { noremap = true, silent = true, expr = false })
 
   -- -- Extract block doesn't need visual mode
   -- vim.api.nvim_set_keymap("n", "<leader>rb", [[ <Cmd>lua require('refactoring').refactor('Extract Block')<CR>]],
@@ -66,13 +66,13 @@ end
 ------------------------------------------------------------ Tree plugin {{{1
 do
   require "nvim-tree".setup {
-    hijack_netrw = false,
-    respect_buf_cwd = true,
-    sort_by = 'extension',
-    reload_on_bufenter = true,
-    update_focused_file = {
-      enable = true
-    }
+      hijack_netrw = false,
+      respect_buf_cwd = true,
+      sort_by = 'extension',
+      reload_on_bufenter = true,
+      update_focused_file = {
+          enable = true
+      }
   }
 end
 
@@ -81,9 +81,9 @@ do
   require("mason").setup()
   require("mason-lspconfig").setup()
   require("mason-null-ls").setup({
-    -- ensure_installed = nil,
-    -- automatic_installation = true,
-    automatic_setup = true,
+      -- ensure_installed = nil,
+      -- automatic_installation = true,
+      automatic_setup = true,
   })
 
   -- You will likely want to reduce updatetime which affects CursorHold
@@ -119,110 +119,139 @@ do
       hi! LspReferenceWrite cterm=bold ctermbg=red guibg=#403000
     ]]
       vim.api.nvim_create_augroup('lsp_document_highlight', {
-        clear = false
+          clear = false
       })
       vim.api.nvim_clear_autocmds({
-        buffer = bufnr,
-        group = 'lsp_document_highlight',
+          buffer = bufnr,
+          group = 'lsp_document_highlight',
       })
       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-        group = 'lsp_document_highlight',
-        buffer = bufnr,
-        callback = vim.lsp.buf.document_highlight,
+          group = 'lsp_document_highlight',
+          buffer = bufnr,
+          callback = vim.lsp.buf.document_highlight,
       })
       vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-        group = 'lsp_document_highlight',
-        buffer = bufnr,
-        callback = vim.lsp.buf.clear_references,
+          group = 'lsp_document_highlight',
+          buffer = bufnr,
+          callback = vim.lsp.buf.clear_references,
       })
     end
   end
 
   require "mason-lspconfig".setup_handlers {
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function(server_name) -- default handler (optional)
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      require("lspconfig")[server_name].setup {
-        on_attach = lspconfig_on_attach,
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { 'vim' }, -- Get the language server to recognize the `vim` global
+      -- The first entry (without a key) will be the default handler
+      -- and will be called for each installed server that doesn't have
+      -- a dedicated handler.
+      function(server_name) -- default handler (optional)
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        require("lspconfig")[server_name].setup {
+            on_attach = lspconfig_on_attach,
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { 'vim' }, -- Get the language server to recognize the `vim` global
+                    },
+                },
+                haskell = {
+                    hlintOn = true,
+                    formattingProvider = "fourmolu"
+                },
             },
-          },
-          haskell = {
-            hlintOn = true,
-            formattingProvider = "fourmolu"
-          },
-        },
-      }
-    end,
-    -- Next, you can provide a dedicated handler for specific servers.
-    -- For example, a handler override for the `rust_analyzer`:
-    ["rust_analyzer"] = function()
-      require("rust-tools").setup {}
+        }
+      end,
+      -- Next, you can provide a dedicated handler for specific servers.
+      -- For example, a handler override for the `rust_analyzer`:
+      ["rust_analyzer"] = function()
+        require("rust-tools").setup {}
+      end
+  }
+
+  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+  local null_ls_on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+          end,
+      })
     end
+    lspconfig_on_attach(client, bufnr)
+  end
+
+  local null_ls = require 'null-ls'
+  null_ls.setup {
+      sources = {
+          null_ls.builtins.completion.spell,
+          null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.diagnostics.credo,
+          null_ls.builtins.diagnostics.golangci_lint,
+          null_ls.builtins.formatting.elm_format,
+          null_ls.builtins.formatting.mix,
+          null_ls.builtins.formatting.prettierd,
+      },
+      on_attach = null_ls_on_attach,
   }
 end
 
 do
   require('mason-tool-installer').setup {
-    -- a list of all tools you want to ensure are installed upon
-    -- start; they should be the names Mason uses for each tool
-    ensure_installed = {
-      -- { '' , version = '1.8.0.0' }, you can pin a tool to a particular version
-      'autopep8',
-      'css-lsp',
-      'diagnostic-languageserver', 'djlint', 'dockerfile-language-server',
-      'elixir-ls', 'elm-format', 'elm-language-server', 'emmet-ls',
-      'flake8',
-      'gitlint', 'gofumpt', 'goimports', 'gopls', 'gotests', 'gotestsum',
-      'haskell-language-server', 'html-lsp',
-      'iferr',
-      'jedi-language-server', 'json-lsp',
-      'lua-language-server', 'luacheck',
-      'markdownlint',
-      'prettierd', 'pylint', 'python-lsp-server',
-      'revive',
-      'semgrep', 'solargraph', 'standardrb', 'staticcheck', 'stylua',
-      'tailwindcss-language-server',
-      'vim-language-server',
-      'yaml-language-server', 'yamlfmt', 'yamllint', 'yapf',
-    },
+      -- a list of all tools you want to ensure are installed upon
+      -- start; they should be the names Mason uses for each tool
+      ensure_installed = {
+          -- { '' , version = '1.8.0.0' }, you can pin a tool to a particular version
+          'autopep8',
+          'css-lsp',
+          'diagnostic-languageserver', 'djlint', 'dockerfile-language-server',
+          'elixir-ls', 'elm-format', 'elm-language-server', 'emmet-ls',
+          'flake8',
+          'gitlint', 'gofumpt', 'goimports', 'gopls', 'gotests', 'gotestsum',
+          'haskell-language-server', 'html-lsp',
+          'iferr',
+          'jedi-language-server', 'json-lsp',
+          'lua-language-server', 'luacheck',
+          'markdownlint',
+          'prettierd', 'pylint', 'python-lsp-server',
+          'revive',
+          'semgrep', 'solargraph', 'standardrb', 'staticcheck', 'stylua',
+          'tailwindcss-language-server',
+          'vim-language-server',
+          'yaml-language-server', 'yamlfmt', 'yamllint', 'yapf',
+      },
   }
 end
 
 ------------------------------------------------------------- Treesitter {{{1
 do
   require 'nvim-treesitter.configs'.setup {
-    ensure_installed = {
-      "bash",
-      "comment", "css",
-      "dockerfile",
-      "eex", "elixir", "elm",
-      "gitcommit", "gitignore", "go",
-      "haskell", "heex", "help", "html",
-      "javascript", "json",
-      "lua",
-      "markdown",
-      "python",
-      "ruby",
-      "vim",
-      "yaml",
-    },
+      ensure_installed = {
+          "bash",
+          "comment", "css",
+          "dockerfile",
+          "eex", "elixir", "elm",
+          "gitcommit", "gitignore", "go",
+          "haskell", "heex", "help", "html",
+          "javascript", "json",
+          "lua",
+          "markdown",
+          "python",
+          "ruby",
+          "vim",
+          "yaml",
+      },
 
-    highlight = {
-      enable = true,
-      disable = {},
-    },
+      highlight = {
+          enable = true,
+          disable = {},
+      },
 
-    indent = {
-      enable = true,
-      disable = {},
-    },
+      indent = {
+          enable = true,
+          disable = {},
+      },
   }
 end
 
@@ -232,110 +261,66 @@ do
   -- Set up nvim-cmp.
   local cmp = require 'cmp'
   cmp.setup {
-    snippet = {
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      end,
-    },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }, { "i", "s", "c" }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ['<TAB>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        else
-          fallback()
-        end
-      end, { "i", "s", "c" }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        else
-          fallback()
-        end
-      end, { "i", "s", "c" }),
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp',
-        ---@diagnostic disable-next-line: unused-local
-        entry_filter = function(entry, ctx)
-          return require('cmp.types').lsp.CompletionItemKind[entry:get_kind()] ~= 'Text'
-        end
+      snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+          end,
       },
-      { name = 'emoji' },
-      { name = 'luasnip' }, -- For luasnip users.
-    }, {
-      { name = 'buffer' },
-    })
+      window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs( -4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }, { "i", "s", "c" }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<TAB>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            else
+              fallback()
+            end
+          end, { "i", "s", "c" }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { "i", "s", "c" }),
+      }),
+      sources = cmp.config.sources({
+          { name = 'nvim_lsp',
+              ---@diagnostic disable-next-line: unused-local
+              entry_filter = function(entry, ctx)
+                return require('cmp.types').lsp.CompletionItemKind[entry:get_kind()] ~= 'Text'
+              end
+          },
+          { name = 'emoji' },
+          { name = 'luasnip' }, -- For luasnip users.
+      }, {
+          { name = 'buffer' },
+      })
   }
 
   -- `/` cmdline setup.
   cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    completion = { autocomplete = false },
-    sources = {
-      {
-        name = 'buffer',
-        option = {
-          keyword_pattern = [=[[^(\v)?[:blank:]].*]=]
-        }
+      mapping = cmp.mapping.preset.cmdline(),
+      completion = { autocomplete = false },
+      sources = {
+          {
+              name = 'buffer',
+              option = {
+                  keyword_pattern = [=[[^(\v)?[:blank:]].*]=]
+              }
+          }
       }
-    }
   })
   -- `:` cmdline setup.
   cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } })
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } })
   })
-end
-
---------------------------------------------------------------- Prettier {{{1
-do
-  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-  local null_ls_on_attach = function(client, bufnr)
-    if client.supports_method "textDocument/formatting" then
-      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end
-
-  local null_ls = require 'null-ls'
-  null_ls.setup {
-    sources = {
-      null_ls.builtins.formatting.prettierd.with {
-        filetypes = {
-          "bash",
-          "comment", "css",
-          "dockerfile",
-          "eex", "elixir", "elm",
-          "gitcommit", "gitignore", "go", "golangci_lint",
-          "haskell", "heex", "help", "html", "stylish_haskell",
-          "javascript", "json", "jq",
-          "lua",
-          "markdown",
-          "python",
-          "ruby",
-          "standardrb",
-          "vim",
-          "yaml",
-        },
-      },
-      null_ls.builtins.diagnostics.eslint,
-      null_ls.builtins.completion.spell,
-    },
-    on_attach = null_ls_on_attach,
-  }
 end
