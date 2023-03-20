dfe1fde9c9ebda7b6082ce72316d5dd1621b5eb0
Cyber Oliveira <lccro@msn.com>
2023-03-20 11:14:31 -0300

    showing nvim-cmp some <3

--- vim.symlink/lua/plugins/cmp.lua
+++ vim.symlink/lua/plugins/cmp.lua
@@ -1,22 +1,22 @@
 vim.o.completeopt = "menu,menuone,noselect"
+
+local lspkind = require("lspkind")
 local types = require("cmp.types")
 local cmp = require("cmp")
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
   window = {
     -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
   mapping = cmp.mapping.preset.insert({
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-j>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }, { "i", "s", "c" }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }, { "i", "s", "c" }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<TAB>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -33,20 +33,35 @@ cmp.setup({
     end, { "i", "s", "c" }),
   }),
   sources = cmp.config.sources({
-    { name = "luasnip" }, -- For luasnip users.
+    { name = "nvim_lua" },
     {
       name = "nvim_lsp",
-      ---@diagnostic disable-next-line: unused-local
-      entry_filter = function(entry, _ctx)
+      entry_filter = function(entry)
         return types.lsp.CompletionItemKind[entry:get_kind()] ~= "Text"
       end,
     },
-  }, {
     { name = "path" },
+    { name = "luasnip" }, -- For luasnip users.
     { name = "buffer", keyword_length = 5 },
   }),
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  formatting = {
+    format = lspkind.cmp_format({
+      menu = {
+        buffer = "[buf]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[api]",
+        path = "[path]",
+        luasnip = "[snip]",
+      },
+    }),
+  },
   experimental = {
-    native_menu = false,
+    ghost_text = true,
   },
 })
 
