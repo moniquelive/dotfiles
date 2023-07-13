a8bc42f0eb88230070763ec2f1a58702ee0487c2
Cyber Oliveira <lccro@msn.com>
2023-07-13 10:52:05 -0300

    feat(emacs): adds init.el

---
+++ config/emacs.symlink/init.el
@@ -0,0 +1,66 @@
+(require 'package)
+(add-to-list 'package-archives '("gnu-devel" . "https://elpa.gnu.org/devel/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(make-backup-files nil)
+ '(package-selected-packages '(elisp-autofmt helm evil counsel use-package))
+ '(use-package-always-ensure t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; This is only needed once, near the top of the file
+(eval-when-compile (require 'use-package))
+
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
+
+(use-package counsel)
+(use-package nerd-icons)
+(use-package doom-modeline
+  :init
+  (setq doom-modeline-icon t)
+  :config (doom-modeline-mode 1))
+
+(use-package gruvbox-theme
+  :config
+  (load-theme 'gruvbox-dark-hard t nil))
+
+(use-package evil
+  :config (evil-mode 1))
+
+(use-package helm
+  :init
+  (global-set-key (kbd "M-x") #'helm-M-x)
+  (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+  (global-set-key (kbd "C-x C-f") #'helm-find-files)
+  :config
+  (setq completion-styles '(flex))
+  (helm-mode 1)
+  )
+
+(use-package elisp-autofmt
+  :commands (elisp-autofmt-mode elisp-autofmt-buffer)
+  :hook (emacs-lisp-mode . elisp-autofmt-mode))
+
+;;;; Mouse scrolling in terminal emacs
+(unless (display-graphic-p)
+  ;; activate mouse-based scrolling
+  (xterm-mouse-mode 1)
+  (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
+  (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
+  )
+
