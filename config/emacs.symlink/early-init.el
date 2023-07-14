eb9b1160b9918cbcc83763b9cfe35f28c83ac3db
Cyber Oliveira <lccro@msn.com>
2023-07-14 14:00:07 -0300

    emacs config split

---
+++ config/emacs.symlink/early-init.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(make-backup-files nil)
+ '(package-selected-packages '(elisp-autofmt helm evil counsel use-package))
+ '(save-place-mode t)
+ '(use-package-always-ensure t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
--- config/emacs.symlink/init.el
+++ config/emacs.symlink/init.el
@@ -1,26 +1,13 @@
 (require 'package)
 (add-to-list 'package-archives '("gnu-devel" . "https://elpa.gnu.org/devel/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
 (package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(make-backup-files nil)
- '(package-selected-packages '(elisp-autofmt helm evil counsel use-package))
- '(use-package-always-ensure t))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-;; This is only needed once, near the top of the file
-(eval-when-compile (require 'use-package))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package)) ;; This is only needed once, near the top of the file
 
 (use-package auto-package-update
   :config
@@ -63,4 +50,3 @@
   (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
   (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
   )
-
