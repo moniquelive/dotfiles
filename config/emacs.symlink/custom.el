2808b73e449745dc7622382516be1aae03ad5d84
Cyber Oliveira <lccro@msn.com>
2023-07-21 15:07:07 -0300

    chore: updates

--- config/emacs.symlink/custom.el
+++ config/emacs.symlink/custom.el
@@ -3,8 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(desktop-save-mode t)
- '(helm-minibuffer-history-key "M-p")
  '(make-backup-files nil)
  '(package-selected-packages
    '(pyvenv helpful treemacs-magit magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs csharp-mode dashboard elixir-mode evil-surround evil-leader lsp-pyright which-key helm-lsp modus-themes projectile evil-collection lsp-ui company-box company lsp-haskell go-mode elisp-autofmt nerd-icons doom-modeline auto-package-update helm evil use-package))
