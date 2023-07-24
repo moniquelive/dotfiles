e9ec106287b07a6d230ed9f05c07fcf9343b714b
Cyber Oliveira <lccro@msn.com>
2023-07-24 12:17:06 -0300

    emacs startup profiling

--- config/emacs.symlink/init.el
+++ config/emacs.symlink/init.el
@@ -1,5 +1,13 @@
-;; first line please
-(setq gc-cons-threshold (* 50 1000 1000))
+(load "~/.config/emacs/gcmh.el")
+(gcmh-mode 1)
+
+(defun my-message-with-timestamp (old-func fmt-string &rest args)
+   "Prepend current timestamp (with microsecond precision) to a message"
+   (apply old-func
+          (concat (format-time-string "[%F %T.%3N %Z] ")
+                   fmt-string)
+          args))
+(advice-add 'message :around #'my-message-with-timestamp)
 
 (require 'package)
 (add-to-list 'package-archives '("gnu-devel" . "https://elpa.gnu.org/devel/"))
@@ -11,6 +19,7 @@
 (setq custom-file "~/.config/emacs/custom.el")
 (load custom-file)
 
+;; GLOBAL settings
 (global-hl-line-mode 1)
 (global-auto-revert-mode 1)
 (tool-bar-mode -1)
@@ -23,25 +32,25 @@
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(setq use-package-always-ensure t)
-(setq use-package-verbose t)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile (require 'use-package)) ;; This is only needed once, near the top of the file
-
 ;;;; Mouse scrolling in terminal emacs
 (unless (display-graphic-p)
   ;; activate mouse-based scrolling
   (xterm-mouse-mode 1)
   (global-set-key (kbd "<mouse-4>") 'scroll-down-line)
-  (global-set-key (kbd "<mouse-5>") 'scroll-up-line)
-  )
+  (global-set-key (kbd "<mouse-5>") 'scroll-up-line))
+
+;; Packages
+(setq use-package-always-ensure t)
+;; (setq use-package-verbose t)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package)) ;; This is only needed once, near the top of the file
 
 (use-package auto-package-update
   :config
   (setq auto-package-update-delete-old-versions t
-	auto-package-update-hide-results t)
+        auto-package-update-hide-results t)
   (auto-package-update-maybe))
 
 (use-package rainbow-delimiters :hook (prog-mode . rainbow-delimiters-mode))
@@ -49,90 +58,100 @@
 (use-package modus-themes
   :config
   (setq modus-themes-italic-constructs t
-	modus-themes-bold-constructs t
-	modus-themes-mixed-fonts t
-	modus-themes-prompts '(italic bold)
-	)
+        modus-themes-bold-constructs t
+        modus-themes-mixed-fonts t
+        modus-themes-prompts '(italic bold))
   (load-theme 'modus-vivendi-tinted :no-confirm)
   (define-key global-map (kbd "<f5>") #'modus-themes-toggle))
 
 (use-package doom-modeline
-  :init (setq doom-modeline-icon t)
-  :config (doom-modeline-mode 1))
+  :config (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
 (use-package which-key
   :defer 0
   :diminish which-key-mode
+  :custom
+    ((which-key-popup-type 'side-window)
+     (which-key-side-window-location '(right bottom))
+     (which-key-side-window-max-width 0.5))
   :config
-  (which-key-setup-side-window-right-bottom)
-  (which-key-mode))
+    (which-key-mode))
 
 (use-package helpful
   :commands (helpful-callable helpful-variable helpful-command helpful-key)
   :custom
   (counsel-describe-function-function #'helpful-callable)
   (counsel-describe-variable-function #'helpful-variable)
-  :init
+  :config
   (global-set-key (kbd "C-h f") #'helpful-callable)
   (global-set-key (kbd "C-h v") #'helpful-variable)
   (global-set-key (kbd "C-h k") #'helpful-key)
   (global-set-key (kbd "C-h x") #'helpful-command))
 
 (use-package evil
-  :after lsp-mode
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
   :custom
-  (evil-split-window-below t)
-  (evil-undo-system 'undo-redo)
-  (evil-vsplit-window-right t)
+    (evil-split-window-below t)
+    (evil-undo-system 'undo-redo)
+    (evil-vsplit-window-right t)
+    (evil-want-integration t)
+    (evil-want-keybinding nil)
   :config
-  (evil-mode 1)
-  (evil-set-undo-system 'undo-redo)
-  (evil-define-key 'normal 'lsp-mode-map "K" 'lsp-ui-doc-glance)
-  (evil-define-key 'normal 'lsp-mode-map "gr" 'lsp-find-references))
+    (evil-mode 1)
+    (evil-set-undo-system 'undo-redo))
 (use-package evil-leader
   :after evil
   :config
-  (global-evil-leader-mode)
-  (evil-leader/set-key
-   (kbd "RET") 'evil-ex-nohighlight
-   "\\" 'evil-buffer))
+    (global-evil-leader-mode)
+    (evil-leader/set-key
+    (kbd "RET") 'evil-ex-nohighlight
+    "\\" 'evil-buffer))
 (use-package evil-surround
   :after evil
   :config (global-evil-surround-mode 1))
 (use-package evil-collection
   :after evil
-  :config
-  (evil-collection-init))
+  :config (evil-collection-init))
 
 (use-package helm
   :diminish
-  :init
-  (global-set-key (kbd "M-x") #'helm-M-x)
-  (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-  (global-set-key (kbd "C-x C-f") #'helm-find-files)
+  :bind
+    (( "M-x" . helm-M-x)
+    ( "C-x b" . helm-buffers-list)
+    ( "C-x c o" . helm-occur)		;SC
+    ( "M-y" . helm-show-kill-ring)	;SC
+    ( "C-x r b" . helm-filtered-bookmarks)
+    ( "C-x C-f" . helm-find-files))
   :custom
-  (helm-minibuffer-history-key "M-p")
+    (helm-minibuffer-history-key "M-p")
   :config
-  (setq completion-styles '(flex))
-  (helm-mode 1))
+    (setq completion-styles '(flex))
+    (helm-mode 1))
 
 (use-package elisp-autofmt
   :commands (elisp-autofmt-mode elisp-autofmt-buffer)
   :hook (emacs-lisp-mode . elisp-autofmt-mode))
 
 (use-package lsp-mode
-  :init (setq lsp-keymap-prefix "C-c l") ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  :config (lsp-enable-which-key-integration t)
-  :commands (lsp lsp-deferred))
+  :commands (lsp lsp-deferred)
+  :init (setq lsp-keymap-prefix "C-c l")
+  :hook (
+         (go-mode . lsp)
+         (haskell-mode . lsp)
+         (elixir-mode . lsp)
+         (csharp-mode . lsp)
+         (python-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :config
+    (lsp-enable-which-key-integration t)
+    (evil-define-key 'normal 'lsp-mode-map "K" 'lsp-ui-doc-glance)
+    (evil-define-key 'normal 'lsp-mode-map "gr" 'lsp-find-references))
 (use-package lsp-ui
   :custom (lsp-ui-doc-position 'at-point)
   :hook (lsp-mode . lsp-ui-mode))
 (use-package helm-lsp
-  :after lsp-mode
-  :init (define-key lsp-mode-map [remap xref-find-apropos] #'helm-lsp-workspace-symbol))
+  :after lsp
+  :config (define-key lsp-mode-map [remap xref-find-apropos] #'helm-lsp-workspace-symbol))
 (use-package csharp-mode
   :custom (lsp-csharp-omnisharp-roslyn-binary-path "/Users/cyber/.dotnet/omnisharp/OmniSharp")
   :hook (csharp-mode . lsp-deferred))
@@ -143,7 +162,11 @@
   :hook (go-mode . lsp-deferred))
 (use-package lsp-haskell
   :hook (haskell-mode . lsp-deferred))
+(use-package python
+  :mode ("\\.py\\'" . python-mode)
+  :interpreter ("python" . python-mode))
 (use-package lsp-pyright
+  :after python-mode
   :hook (python-mode . (lambda ()
 			 (require 'lsp-pyright)
 			 (lsp-deferred))))
@@ -154,33 +177,34 @@
   :after lsp-mode
   :hook (lsp-mode . company-mode)
   :bind (:map company-active-map
-	      ("C-n" . company-select-next)
-	      ("C-p" . company-select-previous)
-	      ("<tab>" . company-complete-selection))
-  (:map lsp-mode-map
-	("<tab>" . company-indent-or-complete-common))
+          ("C-n" . company-select-next)
+          ("C-p" . company-select-previous)
+          ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+          ("<tab>" . company-indent-or-complete-common))
   :custom
-  (company-idle-delay 0.3)
-  (company-minimum-prefix-length 3))
+    (company-idle-delay 0.3)
+    (company-minimum-prefix-length 3))
 (use-package company-box
   :hook (company-mode . company-box-mode))
 
 (use-package projectile
   :diminish projectile-mode
   :init
-  (when (file-directory-p "~/prj")
-    (setq projectile-project-search-path '("~/prj")))
-  (setq projectile-switch-project-action #'projectile-dired)
-  :bind-keymap (("s-p" . projectile-command-map)
-		("C-c p" . projectile-command-map))
+    (when (file-directory-p "~/prj")
+      (setq projectile-project-search-path '("~/prj")))
+    (setq projectile-switch-project-action #'projectile-dired)
+  :bind-keymap
+    (("s-p" . projectile-command-map)
+		 ("C-c p" . projectile-command-map))
   :config (projectile-mode))
 
 (use-package dashboard
   :init (setq dashboard-projects-backend 'projectile
 	      dashboard-items '((recents . 10)
-				(bookmarks . 5)
-				(projects . 5)
-				(registers . 5))
+                          (bookmarks . 5)
+                          (projects . 5)
+                          (registers . 5))
 	      dashboard-set-navigator t
 	      dashboard-center-content t
 	      dashboard-display-icons-p t
@@ -290,6 +314,4 @@
 (use-package treemacs-icons-dired :hook (dired-mode . treemacs-icons-dired-enable-once))
 (use-package treemacs-magit :after (treemacs magit))
 
-;; Make gc pauses faster by decreasing the threshold.
-;; last line please
-(setq gc-cons-threshold (* 2 1000 1000))
+(advice-remove 'message #'my-message-with-timestamp)
