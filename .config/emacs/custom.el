1be19a2cbfde4876bc46455296b0d8f0e65ad077
Cyber Oliveira <lccro@msn.com>
2023-07-29 10:00:29 -0300

    feat/chore: evil-nerd-commenter, ligatures, save-on-focus-lost...
    consult, save-undo, elm-mode, lsp-ui customizations

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(make-backup-files nil)
  '(package-selected-packages
-   '(orderless marginalia vertico lsp-mode pyvenv helpful treemacs-magit magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs csharp-mode dashboard elixir-mode evil-surround evil-leader lsp-pyright which-key helm-lsp modus-themes projectile evil-collection lsp-ui company-box company lsp-haskell go-mode elisp-autofmt nerd-icons doom-modeline auto-package-update helm evil use-package))
+   '(evil-nerd-commenter elm-mode consult orderless marginalia vertico lsp-mode pyvenv helpful treemacs-magit magit treemacs-icons-dired treemacs-projectile treemacs-evil treemacs csharp-mode dashboard elixir-mode evil-surround evil-leader lsp-pyright which-key helm-lsp modus-themes projectile evil-collection lsp-ui company-box company lsp-haskell go-mode elisp-autofmt nerd-icons doom-modeline auto-package-update helm evil use-package))
  '(save-place-mode t))
 
 (custom-set-faces
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -28,10 +28,38 @@
   (set-face-attribute 'default nil
 		      :family "JetBrains Mono"
 		      :height 150) ;; default font size (point * 10)
+  (defconst jetbrains-ligature-mode--ligatures
+    '("-->" "//" "/**" "/*" "*/" "<!--" ":=" "->>" "<<-" "->" "<-"
+      "<=>" "==" "!=" "<=" ">=" "=:=" "!==" "&&" "||" "..." ".."
+      "|||" "///" "&&&" "===" "++" "--" "=>" "|>" "<|" "||>" "<||"
+      "|||>" "<|||" ">>" "<<" "::=" "|]" "[|" "{|" "|}"
+      "[<" ">]" ":?>" ":?" "/=" "[||]" "!!" "?:" "?." "::"
+      "+++" "??" "###" "##" ":::" "####" ".?" "?=" "=!=" "<|>"
+      "<:" ":<" ":>" ">:" "<>" "***" ";;" "/==" ".=" ".-" "__"
+      "=/=" "<-<" "<<<" ">>>" "<=<" "<<=" "<==" "<==>" "==>" "=>>"
+      ">=>" ">>=" ">>-" ">-" "<~>" "-<" "-<<" "=<<" "---" "<-|"
+      "<=|" "/\\" "\\/" "|=>" "|~>" "<~~" "<~" "~~" "~~>" "~>"
+      "<$>" "<$" "$>" "<+>" "<+" "+>" "<*>" "<*" "*>" "</>" "</" "/>"
+      "<->" "..<" "~=" "~-" "-~" "~@" "^=" "-|" "_|_" "|-" "||-"
+      "|=" "||=" "#{" "#[" "]#" "#(" "#?" "#_" "#_(" "#:" "#!" "#="
+      "&="))
+
+  (sort jetbrains-ligature-mode--ligatures (lambda (x y) (> (length x) (length y))))
+
+  (dolist (pat jetbrains-ligature-mode--ligatures)
+    (set-char-table-range composition-function-table
+			  (aref pat 0)
+			  (nconc (char-table-range composition-function-table (aref pat 0))
+				 (list (vector (regexp-quote pat)
+                                               0
+					       'compose-gstring-for-graphic)))))
   (setq dired-use-ls-dired t
         insert-directory-program "/opt/homebrew/bin/gls"
         dired-listing-switches "-aBhl --group-directories-first"))
 (add-hook 'dired-mode-hook 'dired-hide-details-mode)
+
+;; autosave on buffer focus lost
+(add-hook 'focus-out-hook (lambda () (save-some-buffers t)))
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
@@ -98,10 +126,113 @@
   (global-set-key (kbd "C-h k") #'helpful-key)
   (global-set-key (kbd "C-h x") #'helpful-command))
 
+(use-package consult
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)
+         ("M-s D" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  (autoload 'projectile-project-root "projectile")
+  (setq consult-project-function (lambda (_) (projectile-project-root))))
+
 (use-package evil
   :custom
     (evil-want-keybinding nil)
     (evil-want-integration t)
+    (undo-tree-auto-save-history t)
     (evil-undo-system 'undo-redo)
     (evil-split-window-below t)
     (evil-vsplit-window-right t)
@@ -110,7 +241,8 @@
   :config
     (evil-mode 1)
     (evil-global-set-key 'normal "-" 'dired-jump)
-    (evil-set-undo-system 'undo-redo))
+    (evil-define-key 'normal 'prog-mode-map "gcc" 'evilnc-comment-or-uncomment-lines)
+    (evil-define-key 'visual 'prog-mode-map "gc" 'evilnc-comment-or-uncomment-lines))
 (use-package evil-leader
   :after evil
   :config
@@ -124,6 +256,8 @@
 (use-package evil-collection
   :after evil
   :config (evil-collection-init))
+(use-package evil-nerd-commenter
+  :after evil)
 
 (use-package vertico
   :init
@@ -182,19 +316,23 @@
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
   :init (setq lsp-keymap-prefix "C-c l")
-  :hook (
-         (go-mode . lsp)
-         (haskell-mode . lsp)
-         (elixir-mode . lsp)
-         (csharp-mode . lsp)
-         (python-mode . lsp)
-         (lsp-mode . lsp-enable-which-key-integration))
+  :hook
+  (go-mode . lsp)
+  (haskell-mode . lsp)
+  (elixir-mode . lsp)
+  (csharp-mode . lsp)
+  (python-mode . lsp)
+  (lsp-mode . lsp-enable-which-key-integration)
   :config
-    (lsp-enable-which-key-integration t)
-    (evil-define-key 'normal 'lsp-mode-map "K" 'lsp-ui-doc-glance)
-    (evil-define-key 'normal 'lsp-mode-map "gr" 'lsp-find-references))
+  (lsp-enable-which-key-integration t)
+  (evil-define-minor-mode-key 'normal 'lsp-mode "K" 'lsp-ui-doc-glance)
+  (evil-define-minor-mode-key 'normal 'lsp-mode "gr" 'lsp-find-references))
 (use-package lsp-ui
-  :custom (lsp-ui-doc-position 'at-point)
+  :custom
+    (lsp-ui-doc-position 'at-point)
+    (lsp-ui-sideline-enable t)
+    (lsp-ui-sideline-show-diagnostics t)
+    (lsp-ui-sideline-show-hover nil)
   :hook (lsp-mode . lsp-ui-mode))
 (use-package csharp-mode
   :custom (lsp-csharp-omnisharp-roslyn-binary-path "/Users/cyber/.dotnet/omnisharp/OmniSharp")
@@ -211,6 +349,10 @@
   :custom
   (lsp-haskell-server-path "~/.ghcup/bin/haskell-language-server-wrapper")
   :hook (haskell-mode . lsp-deferred))
+(use-package elm-mode
+  :mode ("\\.elm\\'" . elm-mode)
+  :hook (elm-mode . lsp-deferred)
+	(elm-mode . elm-format-on-save-mode))
 (use-package python
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode))
