8beda877fba21a352dcdb7b959c5fb33c3e64c43
Cyber Oliveira <lccro@msn.com>
2023-08-10 15:07:12 -0300

    fix(startup): updates background color to match current theme

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,4 +1,4 @@
-(push '(background-color . "#19202f") default-frame-alist)
+(push '(background-color . "#222134") default-frame-alist)
 (push '(foreground-color . "#f0f0f0") default-frame-alist)
 
 (when (fboundp 'startup-redirect-eln-cache)
