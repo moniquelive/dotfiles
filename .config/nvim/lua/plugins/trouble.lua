0e67688018bba4e1e5c2fc45beb404bfcf831240
Cyber Oliveira <lccro@msn.com>
2024-01-30 18:37:24 -0300

    feat: new trouble-nvim mappings

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,14 +1,26 @@
 vim.cmd([[autocmd! FileType Trouble set wrap]])
+
+local function next()
+	require("trouble").next({ skip_groups = true, jump = true })
+end
+
+local function previous()
+	require("trouble").previous({ skip_groups = true, jump = true })
+end
+
 return {
 	{
 		"folke/trouble.nvim",
 		dependencies = { "kyazdani42/nvim-web-devicons" },
+		cmds = "TroubleToggle",
 		keys = {
+			{ "]t", next },
+			{ "[t", previous },
 			{ "<leader>xx", "<cmd>TroubleToggle<cr>" },
-			{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
-			{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
-			{ "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
-			{ "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
+			-- { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
+			-- { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
+			-- { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
+			-- { "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
 			--{ "gR", "<cmd>TroubleToggle lsp_references<cr>" },
 		},
 	},
