0f9f45cdd2a88074d0c33d6623c52deeac4fc8f3
Cyber Oliveira <lccro@msn.com>
2024-01-30 18:52:12 -0300

    feat(emacs): conditional variable pitch font

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -7,8 +7,13 @@
  '(package-selected-packages
    '(company-posframe pdf-tools evil-org org-superstar company-box company auto-package-update auto-virtualenv consult csharp-mode dashboard dockerfile-mode doom-modeline elisp-autofmt elixir-mode elm-mode embark embark-consult evil evil-collection evil-leader evil-nerd-commenter evil-search-highlight-persist evil-surround go-mode helpful lsp-docker lsp-haskell lsp-mode lsp-pyright lsp-ui magit marginalia nerd-icons orderless projectile projectile-ripgrep pyvenv undo-tree vertico which-key)))
 
+(defvar var-font-name
+  (cond ((find-font (font-spec :name "Helvetica")) "Helvetica 14")
+        ((find-font (font-spec :name "Arial")) "Arial 14")
+        t nil))
+
 (custom-set-faces
  '(default ((t (:font "Monaspace Neon Var 14"))))
  '(fixed-pitch ((t (:inherit (default)))))
  '(fixed-pitch-serif ((t (:inherit (default)))))
- '(variable-pitch ((t (:font "Helvetica 14")))))
+ '(variable-pitch ((t `(:font ,var-font-name)))))
