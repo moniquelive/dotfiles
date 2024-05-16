8f26b514035469e594654a2c0fbdab9276b3fa81
Cyber Oliveira <lccro@msn.com>
2024-05-16 10:28:52 -0300

    chore: updates

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -91,10 +91,10 @@ return {
 		dependencies = {
 			"onsails/lspkind.nvim",
 			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lua",
+			-- "hrsh7th/cmp-nvim-lua",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
+			-- "hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-nvim-lsp-signature-help",
 			"windwp/nvim-autopairs",
 			"saadparwaiz1/cmp_luasnip",
