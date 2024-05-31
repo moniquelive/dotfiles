6dca37a4101f2f0c427e3334c33109cb7e9517e2
Cyber Oliveira <lccro@msn.com>
2024-05-31 17:42:28 -0300

    feat(nvim): major trouble.nvim upgrade

---
+++ .config/nvim/ftplugin/trouble.lua
@@ -0,0 +1,3 @@
+vim.schedule(function()
+	vim.o.wrap = true
+end)
--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,27 +1,13 @@
-vim.cmd([[autocmd! FileType Trouble set wrap]])
-
-local function next()
-	require("trouble").next({ skip_groups = true, jump = true })
-end
-
-local function previous()
-	require("trouble").previous({ skip_groups = true, jump = true })
-end
-
 return {
 	{
 		"folke/trouble.nvim",
 		dependencies = { "kyazdani42/nvim-web-devicons" },
-		cmd = "TroubleToggle",
+		opts = {},
+		cmd = "Trouble",
 		keys = {
-			{ "]t", next },
-			{ "[t", previous },
-			{ "<leader>xx", "<cmd>TroubleToggle<cr>" },
-			-- { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>" },
-			-- { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>" },
-			-- { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>" },
-			-- { "<leader>xl", "<cmd>TroubleToggle loclist<cr>" },
-			--{ "gR", "<cmd>TroubleToggle lsp_references<cr>" },
+			{ "]t", "<cmd>Trouble diagnostics next focus=true<cr>" },
+			{ "[t", "<cmd>Trouble diagnostics prev focus=true<cr>" },
+			{ "<leader>xx", "<cmd>Trouble diagnostics toggle focus=true<cr>" },
 		},
 	},
 }
