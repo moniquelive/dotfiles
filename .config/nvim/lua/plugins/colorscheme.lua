a6d893c495b3a6227f695a25e1bd3451c307f518
Cyber Oliveira <lccro@msn.com>
2024-06-04 12:50:19 -0300

    feat(nvim+colorscheme): gruvbox again

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,22 @@
 return {
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
+		"ellisonleao/gruvbox.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require("rose-pine").setup({ dark_variant = "moon" })
-			vim.o.background = "dark"
-			vim.cmd.colorscheme("rose-pine-main")
-			vim.cmd.highlight([[StatusLine guifg=#ea9a97 guibg=#2a283e]])
+			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	name = "rose-pine",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("rose-pine").setup({ dark_variant = "moon" })
+	-- 		vim.o.background = "dark"
+	-- 		vim.cmd.colorscheme("rose-pine-main")
+	-- 		vim.cmd.highlight([[StatusLine guifg=#ea9a97 guibg=#2a283e]])
+	-- 	end,
+	-- },
 }
