883af9280a33daef3a3c15e0a266c89725022643
Cyber Oliveira <lccro@msn.com>
2024-06-25 11:18:15 -0300

    feat(color): catppuccin mocha color scheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,21 @@
 return {
 	{
-		"ellisonleao/gruvbox.nvim",
-		lazy = false,
+		"catppuccin/nvim",
+		name = "catppuccin",
 		priority = 1000,
+		lazy = false,
 		config = function()
-			vim.cmd.colorscheme("gruvbox")
+			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
 	},
+	-- {
+	-- 	"ellisonleao/gruvbox.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("gruvbox")
+	-- 	end,
+	-- },
 	-- {
 	-- 	"rose-pine/neovim",
 	-- 	name = "rose-pine",
