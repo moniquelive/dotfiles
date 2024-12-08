d9ff802b6964642b56fe83698d03fc8a2d748bf7
Cyber Oliveira <lccro@msn.com>
2024-12-08 10:39:32 -0300

    feat(treesitter): simplify

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,30 @@ vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 
 return {
 	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		main = "nvim-treesitter.configs",
+		opts = {
+			ensure_installed = "all",
+			ignore_install = { "unison" },
+			auto_install = true,
+			endwise = { enable = true },
+			highlight = { enable = true, additional_vim_regex_highlighting = { "ruby" } },
+			indent = { enable = true, disable = { "python", "ruby" } },
+			textobjects = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<M-UP>", -- set to `false` to disable one of the mappings
+					node_incremental = "<M-UP>",
+					scope_incremental = "<M-RIGHT>",
+					node_decremental = "<M-DOWN>",
+				},
+			},
+			matchup = { enable = true }, -- mandatory, false will disable the whole extension ,
+		},
+	},
+	{
 		"nvim-treesitter/nvim-treesitter-refactor",
 		main = "nvim-treesitter.configs",
 		opts = {
@@ -12,28 +36,6 @@ return {
 				navigation = { enable = true, goto_definition_lsp_fallback = true },
 			},
 		},
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			build = ":TSUpdate",
-			main = "nvim-treesitter.configs",
-			opts = {
-				ensure_installed = "all",
-				auto_install = true,
-				endwise = { enable = true },
-				highlight = { enable = true, additional_vim_regex_highlighting = { "ruby" } },
-				indent = { enable = true, disable = { "python", "ruby" } },
-				textobjects = { enable = true },
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<M-UP>", -- set to `false` to disable one of the mappings
-						node_incremental = "<M-UP>",
-						scope_incremental = "<M-RIGHT>",
-						node_decremental = "<M-DOWN>",
-					},
-				},
-				matchup = { enable = true }, -- mandatory, false will disable the whole extension ,
-			},
-		},
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
 	},
 }
