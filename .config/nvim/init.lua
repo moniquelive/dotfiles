10bd6e6283f05bc233c133baa9f8f52386bc5988
Cyber Oliveira <lccro@msn.com>
2024-12-15 11:17:02 -0300

    feat(vimgrep): new shortcut

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -187,6 +187,7 @@ endfunction
 command! -nargs=+ -complete=file_in_path -bar Grep  cgetexpr Grep(<f-args>)
 command! -nargs=+ -complete=file_in_path -bar LGrep lgetexpr Grep(<f-args>)
 
+cnoreabbrev <expr> gr    (getcmdtype() ==# ':' && getcmdline() ==# 'grep')  ? 'Grep'  : 'grep'
 cnoreabbrev <expr> grep  (getcmdtype() ==# ':' && getcmdline() ==# 'grep')  ? 'Grep'  : 'grep'
 cnoreabbrev <expr> lgrep (getcmdtype() ==# ':' && getcmdline() ==# 'lgrep') ? 'LGrep' : 'lgrep'
 
