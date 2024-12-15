fcb81de92b3f6658507286101285c0cb6d22711d
Cyber Oliveira <lccro@msn.com>
2024-12-15 12:06:38 -0300

    feat(nvim+run): politer mapping for lua/love

--- .config/nvim/ftplugin/lua.lua
+++ .config/nvim/ftplugin/lua.lua
@@ -1,5 +1,5 @@
 vim.opt_local.makeprg = "lua %"
 
-if vim.fn.executable("love") == 1 then
-	vim.keymap.set("n", "<f5>", "<cmd>!love .<cr>", { silent = true })
+if vim.fn.executable("love") == 1 and vim.fn.search("function love", "n") > 0 then
+	vim.keymap.set("n", "<f5>", "<cmd>!love .<cr>", { silent = true, buffer = true })
 end
