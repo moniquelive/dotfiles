fece6ee354e0d04f4c22f7bee21bcae758403be7
Cyber Oliveira <lccro@msn.com>
2024-12-15 11:07:59 -0300

    feat(nvim): lua makeprg

--- .config/nvim/ftplugin/lua.lua
+++ .config/nvim/ftplugin/lua.lua
@@ -1,1 +1,5 @@
-vim.keymap.set("n", "<f5>", "<cmd>!love .<cr>", { silent = true })
+vim.opt_local.makeprg = "lua %"
+
+if vim.fn.executable("love") == 1 then
+	vim.keymap.set("n", "<f5>", "<cmd>!love .<cr>", { silent = true })
+end
