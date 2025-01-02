28d9375951b2b11ce6aea468918c60acca0640aa
Cyber Oliveira <lccro@msn.com>
2025-01-02 12:07:20 -0300

    chore(nvim): still figuring out how to make supermaven and blink behave

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,18 +1,27 @@
+local augroup = vim.api.nvim_create_augroup("supermaven-blink", { clear = true })
+
 -- recipes from https://cmp.saghen.dev/recipes.html#hide-copilot-on-suggestion
 vim.api.nvim_create_autocmd('User', {
   pattern = 'BlinkCmpCompletionMenuOpen',
-  callback = function()
+  group = augroup,
+  callback = function(ev)
+    -- TODO: hacky, but works
+    local ns_id = vim.api.nvim_create_namespace("supermaven")
+    vim.api.nvim_buf_del_extmark(ev.buf, ns_id, 1)
+
     local api = require("supermaven-nvim.api")
-    if api.is_running() then
-      require('supermaven-nvim.completion_preview').on_dispose_inlay()
-      api.stop()
-    end
+    if api.is_running() then api.stop() end
   end,
 })
 
 vim.api.nvim_create_autocmd('User', {
   pattern = 'BlinkCmpCompletionMenuClose',
-  callback = function()
+  group = augroup,
+  callback = function(ev)
+    -- TODO: hacky, but works
+    local ns_id = vim.api.nvim_create_namespace("supermaven")
+    vim.api.nvim_buf_del_extmark(ev.buf, ns_id, 1)
+
     local api = require("supermaven-nvim.api")
     if not api.is_running() then api.start() end
   end,
@@ -91,7 +100,7 @@ return {
         }
       },
       documentation = { auto_show = true, auto_show_delay_ms = 200 },
-      ghost_text = { enabled = true },
+      ghost_text = { enabled = false },
       list = {
         selection = function(ctx)
           return ctx.mode == 'cmdline' and 'auto_insert' or 'preselect'
@@ -102,9 +111,9 @@ return {
     signature = { enabled = true },
 
     sources = {
-      default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'lazydev', 'lsp', 'path', 'snippets' }, --, 'buffer' },
       providers = {
-        lsp = { fallbacks = { "lazydev" } }, -- dont show LuaLS require statements when lazydev has items
+        lsp = { fallbacks = { "lazydev" } },              -- dont show LuaLS require statements when lazydev has items
         lazydev = { name = "LazyDev", module = "lazydev.integrations.blink", score_offset = 100 },
       },
     },
