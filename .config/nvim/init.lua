d03770a82f86e8b4eddcca7877048c0133ffe3bf
Monique Oliveira <lccro@msn.com>
2025-04-07 17:53:49 -0300

    chore(nvim): hack to fix telescope for now (vim.o.winborder issue)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -90,6 +90,18 @@ for key, value in pairs(opts) do
   vim.opt[key] = value
 end
 
+-- Temp hack
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TelescopeFindPre",
+  callback = function()
+    vim.opt_local.winborder = "none"
+    vim.api.nvim_create_autocmd("WinLeave", {
+      once = true,
+      callback = function() vim.opt_local.winborder = "rounded" end,
+    })
+  end,
+})
+
 -- Keymaps
 local k = vim.keymap
 
