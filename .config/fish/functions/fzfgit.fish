c62935f202a1a28068744b0079557d08e2580eb8
Monique Oliveira <lccro@msn.com>
2025-04-11 12:12:15 -0300

    chore: updates

--- .config/fish/functions/fzfgit.fish
+++ .config/fish/functions/fzfgit.fish
@@ -1,5 +1,5 @@
 function fzfgit
-    git log --pretty=format:"%h %s (%cr) <%an>" -- $argv | fzf --ansi --no-sort --reverse \
+    git log --pretty=format:"%h %s (%cr) <%an>" -- $argv | fzf --tac --ansi --no-sort --reverse \
         --preview "git show -w -b --color-words --color=always {1} -- $argv" \
         --bind 'enter:accept' | cut -d' ' -f1
 end
