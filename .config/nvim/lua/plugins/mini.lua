0d7941a4402551f722f1d69235d6f9974bce8b08
Monique Oliveira <lccro@msn.com>
2025-06-23 17:43:00 -0300

    feat: getting used to losing the "s" command

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -31,15 +31,15 @@ return {
       })
       require('mini.splitjoin').setup()
       require('mini.surround').setup({
-        mappings = {
-          add = 'gsa',            -- Add surrounding in Normal and Visual modes
-          delete = 'gsd',         -- Delete surrounding
-          find = 'gsf',           -- Find surrounding (to the right)
-          find_left = 'gsF',      -- Find surrounding (to the left)
-          highlight = 'gsh',      -- Highlight surrounding
-          replace = 'gsr',        -- Replace surrounding
-          update_n_lines = 'gsn', -- Update `n_lines`
-        }
+        -- mappings = {
+        --   add = 'gsa',            -- Add surrounding in Normal and Visual modes
+        --   delete = 'gsd',         -- Delete surrounding
+        --   find = 'gsf',           -- Find surrounding (to the right)
+        --   find_left = 'gsF',      -- Find surrounding (to the left)
+        --   highlight = 'gsh',      -- Highlight surrounding
+        --   replace = 'gsr',        -- Replace surrounding
+        --   update_n_lines = 'gsn', -- Update `n_lines`
+        -- }
       })
       require('mini.statusline').setup()
       require('mini.trailspace').setup()
