5568b5b692d0de6716c7e26d0b1fd33b42b7b7b3
Monique Oliveira <lccro@msn.com>
2025-07-12 11:11:20 -0300

    chore: go.nvim tweaks

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -1,12 +1,19 @@
 return {
-	{
-		"ray-x/go.nvim",
-		dependencies = { "ray-x/guihua.lua", },
-		config = true,
-		ft = { "go", "gomod", "gotext", "gohtml" },
-		keys = {
-			{ "<leader>ca", "<cmd>GoCodeLenAct<cr>" },
-		},
-		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+	"ray-x/go.nvim",
+	dependencies = {
+		"ray-x/guihua.lua",
+		"neovim/nvim-lspconfig",
+		"nvim-treesitter/nvim-treesitter",
 	},
+	opts = {
+		golangci_lint = {
+			default = 'standard', -- set to one of { 'standard', 'fast', 'all', 'none' }
+			enable = { 'govet', 'ineffassign', 'revive', 'gosimple' },
+		}
+	},
+	ft = { "go", "gomod", "gotext", "gohtml" },
+	keys = {
+		{ "<leader>ca", "<cmd>GoCodeLenAct<cr>" },
+	},
+	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
