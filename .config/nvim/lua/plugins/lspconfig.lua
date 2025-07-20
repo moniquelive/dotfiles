025e90601d3a87f3a45bcf5a3f845947920dfb7e
Monique Oliveira <lccro@msn.com>
2025-07-20 11:15:47 -0300

    chore: updates

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,8 +2,12 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			vim.lsp.config("elixirls",
-				{ cmd = { vim.fn.expand("~/.local/share/mise/installs/elixir-ls/latest/language_server.sh") } })
+			vim.lsp.config('*', {
+				root_markers = { '.git' },
+			})
+			vim.lsp.config("elixirls", {
+				cmd = { vim.fn.expand("~/.local/share/mise/installs/elixir-ls/latest/language_server.sh") }
+			})
 			vim.lsp.enable({
 				"clangd",
 				"elixirls",
