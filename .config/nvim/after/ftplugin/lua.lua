f1409a66448fba1214ab813caa5c837b0ba459c4
Monique Oliveira <lccro@msn.com>
2025-07-23 14:29:55 -0300

    feat(nvim): lua support for exercism

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -3,3 +3,7 @@ vim.opt_local.makeprg = "lua %"
 if vim.fn.executable("love") == 1 and vim.fn.search("function love", "n") > 0 then
 	vim.keymap.set("n", "<f5>", "<cmd>!love .<cr>", { silent = true, buffer = true })
 end
+
+if string.find(vim.fn.expand("%:p"):lower(), "/exercism/") ~= nil then -- are we exercisming?
+	vim.opt_local.makeprg = "busted"
+end
