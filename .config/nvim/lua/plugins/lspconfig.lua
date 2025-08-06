c634767ecd1bfea39b9f65f3ae01609b987ec96c
Monique Oliveira <lccro@msn.com>
2025-08-06 15:21:25 -0300

    feat(nvim,zoxide): simple lua_ls config + zoxide @ zsh

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -6,9 +6,6 @@ return {
 			vim.lsp.config('*', {
 				root_markers = { '.git' },
 			})
-			vim.lsp.config("lua_ls", {
-				settings = { Lua = { workspace = { library = vim.api.nvim_get_runtime_file("", true) } } }
-			})
 			vim.lsp.config("elixirls", {
 				cmd = { vim.fn.expand("~/.local/share/mise/installs/elixir-ls/latest/language_server.sh") }
 			})
