d631d0473cba84963f065077373e81a8a2beabc7
Monique Oliveira <lccro@msn.com>
2025-08-11 16:44:01 -0300

    chore: lua abbrev && starships aliases

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -1,5 +1,7 @@
 vim.opt_local.makeprg = "lua %"
 
+vim.cmd.iabbrev("tbl", "setmetatable({}, { __index = table })")
+
 if vim.fn.executable("love") == 1 and vim.fn.search("function love", "n") > 0 then
 	vim.keymap.set("n", "<f5>", "<cmd>!love .<cr>", { silent = true, buffer = true })
 end
