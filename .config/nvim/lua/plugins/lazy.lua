52001a12eb3f448ee30cff0278fc715b01e1995d
Monique Oliveira <lccro@msn.com>
2025-10-03 21:40:05 -0300

    chore: refactorings

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -1,6 +1,6 @@
 return {
 	-- misc
-	{ "wincent/terminus",     event = { "BufRead", "BufNewFile" } },
+	{ "wincent/terminus", event = { "BufRead", "BufNewFile" } },
 	-- coding
 	{ "andymass/vim-matchup", keys = "%" },
 	{
@@ -11,12 +11,11 @@ return {
 		opts = { on_save_enabled = true },
 	},
 	{
-		'stevearc/conform.nvim',
+		"stevearc/conform.nvim",
 		event = { "BufWritePre" },
 		cmd = { "ConformInfo" },
 		keys = {
 			{
-				-- Customize or remove this keymap to your liking
 				"<leader>f",
 				function() require("conform").format({ async = true }) end,
 				mode = "",
@@ -24,17 +23,13 @@ return {
 			},
 		},
 		opts = {
-			-- Define your formatters
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "isort", "black" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 			},
-			-- Set default options
 			default_format_opts = { lsp_format = "fallback" },
-			-- Set up format-on-save
 			format_on_save = { timeout_ms = 500 },
-			-- Customize formatters
 			formatters = { shfmt = { append_args = { "-i", "2" } } },
 		},
 	},
@@ -42,18 +37,18 @@ return {
 		"folke/lazydev.nvim",
 		dependencies = {
 			{ "LuaCATS/luassert", name = "luassert-types" },
-			{ "LuaCATS/busted",   name = "busted-types" },
+			{ "LuaCATS/busted", name = "busted-types" },
 		},
 		ft = "lua",
 		opts = {
 			library = {
 				"~/.config/nvim",
 				"rfc.nvim",
-				{ path = "${3rd}/luv/library",     words = { "vim%.uv" } },
-				{ path = "${3rd}/love2d/library",  words = { "love%." } },
-				{ path = "LazyVim",                words = { "LazyVim" } },
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				{ path = "${3rd}/love2d/library", words = { "love%." } },
+				{ path = "LazyVim", words = { "LazyVim" } },
 				{ path = "luassert-types/library", words = { "assert" } },
-				{ path = "busted-types/library",   words = { "describe" } },
+				{ path = "busted-types/library", words = { "describe" } },
 			},
 		},
 	},
