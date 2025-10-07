09103e672ccd8140d9356f11397fd5a266e8466a
Monique Oliveira <lccro@msn.com>
2025-10-07 14:20:52 -0300

    feat(fish): self healing speed command

--- .config/fish/functions/speed.fish
+++ .config/fish/functions/speed.fish
@@ -1,8 +1,20 @@
 function speed
     while true
-        clear
+        printf "Speed testing... "
+        set -l j (speedtest -s 18102 -f json)
+        set -l ds (math (echo $j | jq '.download.bandwidth') \* 8e-6)
+        set -l us (math (echo $j | jq '.upload.bandwidth') \* 8e-6)
         date
-        speedtest -s 18102
+        printf "Download: %s, Upload: %s\n" $ds $us
+        if test $ds -lt 100
+            printf "Restarting network... "
+            sudo networksetup -setnetworkserviceenabled Ethernet off
+            sleep 10
+            sudo networksetup -setnetworkserviceenabled Ethernet on
+            sudo nextdns restart
+            echo Done
+        end
         sleep 300
+        echo ---
     end
 end
