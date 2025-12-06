f7273fd9b4fd3a21b5fcdc93cc3829243c55f6cd
Monique Oliveira <lccro@msn.com>
2025-12-06 14:21:20 -0300

    chore: updates

--- .config/nvim/after/ftplugin/ruby.lua
+++ .config/nvim/after/ftplugin/ruby.lua
@@ -2,3 +2,14 @@ vim.cmd.compiler("ruby")
 vim.opt_local.makeprg = "ruby --yjit %"
 
 vim.cmd.iabbrev("fsl", "# frozen_string_literal: true")
+
+local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+if #lines == 1 and lines[1] == "" then
+	local skeleton = [[#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+]]
+	local skeleton_lines = vim.split(skeleton, "\n")
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, skeleton_lines)
+	vim.api.nvim_win_set_cursor(0, { 4, 1 })
+end
