72b0d66f496082ac8be733b51c8343e4d135528d
Monique Oliveira <lccro@msn.com>
2025-12-07 00:47:36 -0300

    feat(fish): trying out television command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -45,8 +45,12 @@ if status is-interactive
 
     command -q starship; and starship init fish | source; and enable_transience
     command -q mise; and mise activate fish | source
-    command -q fzf; and fzf --fish | source
     command -q zoxide; and zoxide init --cmd cd fish | source
     command -q tailscale; and tailscale completion fish | source
+    if command -q tv
+        tv init fish | source
+    else
+        command -q fzf; and fzf --fish | source
+    end
     command -q carapace; and carapace _carapace | source
 end
