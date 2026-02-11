b12ce0d3ebd48992eb221c77c95fe754ce4ce367
Monique Oliveira <lccro@msn.com>
2026-02-11 10:23:20 -0300

    chore(fish): rearrange config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,26 +9,6 @@ fish_add_path -a $HOME/bin \
     /usr/local/sbin \
     /usr/local/go/bin
 
-if test -s /opt/homebrew/bin/brew
-    set -l brew_prefix (/opt/homebrew/bin/brew --prefix)
-
-    fish_add_path \
-        $brew_prefix/bin \
-        $brew_prefix/opt/node/bin \
-        $brew_prefix/opt/coreutils/bin \
-        $brew_prefix/opt/coreutils/libexec/gnubin \
-        $brew_prefix/opt/python/libexec/bin \
-        $brew_prefix/opt/ruby/bin \
-        $brew_prefix/opt/mysql-client/bin
-
-    set -ax MANPATH $brew_prefix/opt/coreutils/libexec/gnuman
-end
-
-test -f $HOME/.awskeys.sh; and source $HOME/.awskeys.sh
-
-test -z "$SSH_ENV"; and set -xg SSH_ENV $HOME/.ssh/environment
-__ssh_agent_is_started; or __ssh_agent_start
-
 # function starship_transient_prompt_func
 #     starship module -s $status character
 # end
@@ -42,6 +22,24 @@ if status is-interactive
     bind ctrl-p history-prefix-search-backward
     bind down history-prefix-search-forward
     bind up history-prefix-search-backward
+
+    test -f $HOME/.awskeys.sh; and source $HOME/.awskeys.sh
+
+    test -z "$SSH_ENV"; and set -xg SSH_ENV $HOME/.ssh/environment
+    __ssh_agent_is_started; or __ssh_agent_start
+
+    if command -q brew
+        set -l brew_prefix (/opt/homebrew/bin/brew --prefix)
+        set -ax MANPATH $brew_prefix/opt/coreutils/libexec/gnuman
+        fish_add_path \
+            $brew_prefix/bin \
+            $brew_prefix/opt/node/bin \
+            $brew_prefix/opt/coreutils/bin \
+            $brew_prefix/opt/coreutils/libexec/gnubin \
+            $brew_prefix/opt/python/libexec/bin \
+            $brew_prefix/opt/ruby/bin \
+            $brew_prefix/opt/mysql-client/bin
+    end
 
     command -q starship; and starship init fish | source; and enable_transience
     command -q mise; and mise activate fish | source
