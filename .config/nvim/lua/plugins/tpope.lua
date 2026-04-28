5212cf59505bc10a0193e3a8ff229d82ee442ab8
Monique Oliveira <lccro@msn.com>
2026-04-28 18:47:33 -0300

    fix(nvim): load vim-rsi on insert enter

--- .config/nvim/lua/plugins/tpope.lua
+++ .config/nvim/lua/plugins/tpope.lua
@@ -2,6 +2,6 @@
 return {
   { "tpope/vim-characterize", keys = "ga" },
   { "tpope/vim-repeat",       event = { "BufRead", "BufNewFile" } },
-  { "tpope/vim-rsi",          event = "CmdlineEnter" },
+  { "tpope/vim-rsi",          event = { "InsertEnter", "CmdlineEnter" } },
   { "tpope/vim-sleuth",       event = { "BufRead", "BufNewFile" } },
 }
