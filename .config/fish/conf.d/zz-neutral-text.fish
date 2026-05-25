1dbc5b9e10b0e10f9e2f20c5492a3fc89f9fd715
Monique Oliveira <lccro@msn.com>
2026-05-25 10:43:08 -0300

    chore(shell): use neutral text color

---
+++ .config/fish/conf.d/zz-neutral-text.fish
@@ -0,0 +1,9 @@
+# Keep Rose Pine accents, but make regular shell text neutral gray.
+set -l neutral_text d0d0d0
+
+set -g fish_color_normal $neutral_text
+set -g fish_color_command $neutral_text
+set -g fish_color_param $neutral_text
+set -g fish_color_operator $neutral_text
+set -g fish_color_cancel $neutral_text
+set -g fish_color_text $neutral_text
