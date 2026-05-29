b3b4016db73ffd96fa247b8403ea1b1399b75808
Monique Oliveira <lccro@msn.com>
2026-05-29 17:41:47 -0300

    chore: simplified update-modules.sh

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env zsh
 set -e
 
-echo "--- updating:"
+echo "--- updating repo:"
 git pull
 
-echo "--- initting submodules:"
+echo "--- updating submodules:"
 git submodule update --init --recursive
-
-echo "--- pulling latest:"
 git submodule foreach zsh -c '
-  plugins=(git)
-  source $HOME/.oh-my-zsh/oh-my-zsh.sh
-
-  git fetch --all -p
-  git checkout -q $(git_main_branch)
-  git branch
-  git pull -q
+  git fetch --all --prune
+  git remote set-head origin --auto >/dev/null
+  branch=${$(git symbolic-ref --short refs/remotes/origin/HEAD)#origin/}
+  git checkout -q "$branch"
+  git pull --ff-only -q
 '
 
 echo "--- stowing:"
