8e52a5d2e2b08cac391b33e4b9f5f8fc949d9983
Monique Oliveira <lccro@msn.com>
2026-06-05 09:25:32 -0300

    chore(fish): support bubu on more platforms

--- .config/fish/functions/bubu.fish
+++ .config/fish/functions/bubu.fish
@@ -1,13 +1,33 @@
-function bubu
-    brew update
-    and brew outdated
-    and brew upgrade
-    and brew upgrade --greedy
-    and brew upgrade --cask --greedy
-    and brew cleanup
-    and brew autoremove
+function __bubu_mise_update
     mise p up
     and mise prune
     and mise up --bump
     and mise up
+end
+
+function bubu
+    switch (uname -s)
+        case Darwin
+            brew update
+            and brew outdated
+            and brew upgrade
+            and brew upgrade --greedy
+            and brew upgrade --cask --greedy
+            and brew cleanup
+            and brew autoremove
+        case FreeBSD
+            sudo pkg update
+            and sudo pkg upgrade -y
+        case Linux
+            if test -f /etc/arch-release
+                yay -Syuu --noconfirm --devel --needed
+            else
+                echo "bubu: unsupported Linux distribution" >&2
+                return 1
+            end
+        case '*'
+            echo "bubu: unsupported operating system" >&2
+            return 1
+    end
+    and __bubu_mise_update
 end
