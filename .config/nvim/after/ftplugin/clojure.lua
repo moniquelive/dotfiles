71058d9838b68c64e7bbd5d60912e0ab74b782ec
Monique Oliveira <lccro@msn.com>
2026-06-17 19:32:11 -0300

    chore(nvim): lein love

--- .config/nvim/after/ftplugin/clojure.lua
+++ .config/nvim/after/ftplugin/clojure.lua
@@ -1,4 +1,8 @@
 local path = require("config.path")
 
 vim.opt_local.makeprg = "bb %"
-if path.is_exercism() then vim.opt_local.makeprg = "lein test" end
+if path.is_exercism() then
+	vim.opt_local.makeprg = "lein test"
+elseif path.is_lein(vim.fn.expand("%:p:h")) then
+	vim.opt_local.makeprg = "lein run"
+end
--- .config/nvim/lua/config/path.lua
+++ .config/nvim/lua/config/path.lua
@@ -7,4 +7,11 @@ function M.is_exercism(path)
 	return path:find("/exercism/", 1, true) ~= nil
 end
 
+---@param path? string
+---@return boolean
+function M.is_lein(path)
+	path = (path or vim.api.nvim_buf_get_name(0)):lower()
+	return path:find("/src/", 1, true) ~= nil
+end
+
 return M
