10a7d9a6b1d3dde9d76fc1726a2353c757fe6fae
Monique Oliveira <lccro@msn.com>
2026-06-22 16:43:37 -0300

    chore(MonoLisa): updates to the latest

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -34,7 +34,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:font "Monolisa 15"))))
+ '(default ((t (:font "MonoLisaCode 15"))))
  '(fixed-pitch ((t (:inherit (default)))))
  '(fixed-pitch-serif ((t (:inherit (default)))))
  '(variable-pitch ((t `(:font ,var-font-name)))))
