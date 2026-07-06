b68f24cbe8be134a9b79f2d3f574a2772a3ee006
Monique Oliveira <lccro@msn.com>
2026-07-06 17:49:02 -0300

    fix(nvim): skip unsupported Mason tools on FreeBSD

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,3 +1,32 @@
+local function tools_to_install()
+	-- stylua: ignore
+	local tools = {
+		"autopep8", "bash-language-server", "black", "clojure-lsp", "djlint", "dockerfile-language-server",
+		"elm-format", "elm-language-server", "expert", "flake8", "gitlint", "goimports-reviser",
+		"golangci-lint-langserver", "gopls", "iferr", "isort", "json-lsp",
+		"lua-language-server", "markdownlint", "omnisharp", "powershell-editor-services", "prettierd",
+		"pylint", "python-lsp-server", "revive", "ruby-lsp", "rubyfmt",
+		"staticcheck", "stylua", "tailwindcss-language-server", "tombi",
+		"yaml-language-server", "yapf",
+	}
+
+	if vim.uv.os_uname().sysname ~= "FreeBSD" then return tools end
+
+	local unsupported = {
+		["clojure-lsp"] = true,
+		["elm-format"] = true,
+		["expert"] = true,
+		["goimports-reviser"] = true,
+		["lua-language-server"] = true,
+		["omnisharp"] = true,
+		["rubyfmt"] = true,
+		["stylua"] = true,
+		["tombi"] = true,
+	}
+
+	return vim.iter(tools):filter(function(tool) return not unsupported[tool] end):totable()
+end
+
 return {
 	{
 		"williamboman/mason.nvim",
@@ -19,16 +48,7 @@ return {
 			"MasonToolsUpdateSync",
 		},
 		opts = {
-			-- stylua: ignore
-			ensure_installed = {
-				"autopep8", "bash-language-server", "black", "clojure-lsp", "djlint", "dockerfile-language-server",
-				"elm-format", "elm-language-server", "expert", "flake8", "gitlint", "goimports-reviser",
-				"golangci-lint-langserver", "gopls", "iferr", "isort", "json-lsp",
-				"lua-language-server", "markdownlint", "omnisharp", "powershell-editor-services", "prettierd",
-				"pylint", "python-lsp-server", "revive", "ruby-lsp", "rubyfmt",
-				"staticcheck", "stylua", "tailwindcss-language-server", "tombi",
-				"yaml-language-server", "yapf",
-			},
+			ensure_installed = tools_to_install(),
 		},
 	},
 }
