4fc354d78e93f72a6bcced51412f204658b3fb7b
Monique Oliveira <lccro@msn.com>
2026-07-24 13:40:55 -0300

    feat(nvim): adds exercism detection for golang

--- .config/nvim/after/ftplugin/go.lua
+++ .config/nvim/after/ftplugin/go.lua
@@ -1,7 +1,12 @@
+local path = require("config.path")
 local skeleton = require("config.skeleton")
 
 vim.cmd.compiler("go")
-vim.opt_local.makeprg = "go run %:p:S"
+if path.is_exercism() then
+	vim.opt_local.makeprg = "go test -v ."
+elseif path.is_lein(vim.fn.expand("%:p:h")) then
+	vim.opt_local.makeprg = "go run %:p:S"
+end
 
 local bufname = vim.api.nvim_buf_get_name(0)
 if bufname:match("main%.go$") then
