82c70d6b0fa7b9653ba78130baa6654a0d7d0d15
Monique Oliveira <lccro@msn.com>
2026-08-30 11:12:57 -0300

    chore(fish): mise managed fish/fisher

--- .config/fish/conf.d/fish-ssh-agent.fish
+++ .config/fish/conf.d/fish-ssh-agent.fish
@@ -1,11 +1,5 @@
-status is-interactive; or return
-
 if test -z "$SSH_ENV"
     set -xg SSH_ENV $HOME/.ssh/environment
-end
-
-if set -q SSH_AUTH_SOCK[1]; and test -S "$SSH_AUTH_SOCK"
-    return
 end
 
 if not __ssh_agent_is_started
